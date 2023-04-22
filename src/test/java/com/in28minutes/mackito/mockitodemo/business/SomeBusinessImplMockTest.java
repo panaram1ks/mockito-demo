760a98350aae5cb186fc22a8e889d7a5778ba195
evgen <parom.evgen@mail.ru>
2023-04-22 18:35:10 +0300

    Mockito extension @ExtendWith(MockitoExtension.class) @Mock - create dependency @InjectMocks - inject dependency into marked class

--- src/test/java/com/in28minutes/mackito/mockitodemo/business/SomeBusinessImplMockTest.java
+++ src/test/java/com/in28minutes/mackito/mockitodemo/business/SomeBusinessImplMockTest.java
@@ -1,21 +1,39 @@
 package com.in28minutes.mackito.mockitodemo.business;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class SomeBusinessImplMockTest {
 
+    @Mock
+    private DataService dataService;
+
+    @InjectMocks
+    private SomeBusinessImpl businessImpl;
+
     @Test
-    void findTheGreatestFromAllData_basicScenario(){
-        DataService dataServiceMock = mock(DataService.class);
-       // dataServiceMock.retrieveAllData() => new int[]{25,15,5}
-        when(dataServiceMock.retrieveAllData()).thenReturn(new int[]{25,15,5});
-        SomeBusinessImpl someBusiness = new SomeBusinessImpl(dataServiceMock);
-        int theGreatestFromAllData = someBusiness.findTheGreatestFromAllData();
-        assertEquals(25, theGreatestFromAllData);
+    void findTheGreatestFromAllData_basicScenario() {
+        when(dataService.retrieveAllData()).thenReturn(new int[]{25, 15, 5});
+        assertEquals(25, businessImpl.findTheGreatestFromAllData());
+    }
+
+    @Test
+    void findTheGreatestFromAllData_withOneValue() {
+        when(dataService.retrieveAllData()).thenReturn(new int[]{35});
+        assertEquals(35, businessImpl.findTheGreatestFromAllData());
+    }
+
+    @Test
+    void findTheGreatestFromAllData_emptyArray() {
+        when(dataService.retrieveAllData()).thenReturn(new int[]{});
+        assertEquals(Integer.MIN_VALUE, businessImpl.findTheGreatestFromAllData());
     }
 
 }
